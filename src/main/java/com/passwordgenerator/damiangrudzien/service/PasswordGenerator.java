59920c3e164a70ae08925c75d9b2ea769828d657
DamianGrudzien <grudzien1694@gmail.com>
2022-08-13 19:26:37 +0200

    Simple Methods for Generating

--- src/main/java/com/passwordgenerator/damiangrudzien/service/PasswordGenerator.java
+++ src/main/java/com/passwordgenerator/damiangrudzien/service/PasswordGenerator.java
@@ -2,16 +2,39 @@ package com.passwordgenerator.damiangrudzien.service;
 
 import com.passwordgenerator.damiangrudzien.ToDto;
 
-import java.util.Random;
+import java.util.*;
 
 public class PasswordGenerator {
+
+    private static WordService wordServiceLocal;
+
     public static String getRandomPass(WordService wordService) {
+        wordServiceLocal = wordService;
+        return getRandomWord(makeRandomNumbers(2));
+    }
+
+    private static String getRandomWord(List<Integer> integers) {
         StringBuilder password = new StringBuilder();
-        Random random = new Random();
-        int firstRandom = random.nextInt(wordService.findAll().size());
-        int secondRandom = random.nextInt(wordService.findAll().size());
-        password.append(wordService.findById((long) firstRandom).map(ToDto::wordAsDto).orElseThrow(RuntimeException::new).getWord());
-        password.append(wordService.findById((long) secondRandom).map(ToDto::wordAsDto).orElseThrow(RuntimeException::new).getWord());
+        for(int i = 0; i < integers.size(); i++){
+            password.append(wordServiceLocal
+                    .findById((long) integers.get(i))
+                    .map(ToDto::wordAsDto)
+                    .orElseThrow(RuntimeException::new)
+                    .getWord());
+        }
         return password.toString();
+    }
+
+    private static List<Integer> makeRandomNumbers(int amount) {
+        Random random = new Random();
+        List<Integer> randomNumbers = new ArrayList<>();
+        for(int i = 0; i < amount; i++){
+            randomNumbers.add(getRandom(random));
+        }
+        return randomNumbers;
+    }
+
+    private static int getRandom( Random random) {
+        return random.nextInt(wordServiceLocal.findAll().size());
     }
 }
