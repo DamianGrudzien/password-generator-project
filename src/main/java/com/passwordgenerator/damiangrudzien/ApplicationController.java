4a92850a7d2a23cbb18175c2bdb1c86ff55d7b81
DamianGrudzien <grudzien1694@gmail.com>
2022-08-14 11:35:14 +0200

    prepare for heroku

--- src/main/java/com/passwordgenerator/damiangrudzien/ApplicationController.java
+++ src/main/java/com/passwordgenerator/damiangrudzien/ApplicationController.java
@@ -4,29 +4,26 @@ import com.passwordgenerator.damiangrudzien.model.WordDto;
 import com.passwordgenerator.damiangrudzien.service.PasswordGenerator;
 import com.passwordgenerator.damiangrudzien.service.WordService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 public class ApplicationController{
 
     @Autowired
     WordService wordService;
 
-    @RequestMapping("/")
+    @GetMapping("/")
     @ResponseBody
     public String getRoot() {
         return "root";
     }
 
-    @RequestMapping("/passwords/{id}")
-    @ResponseBody
+    @GetMapping("/passwords/{id}")
     public WordDto getPassword(@PathVariable("id") Long id){
         return wordService.findById(id).map(ToDto::wordAsDto).orElseThrow(RuntimeException::new);
     }
 
-    @RequestMapping("/passwords/random")
-    @ResponseBody
+    @GetMapping("/passwords/random")
     public String getRandomPassword(){
         return PasswordGenerator.getRandomPass(wordService);
     }
