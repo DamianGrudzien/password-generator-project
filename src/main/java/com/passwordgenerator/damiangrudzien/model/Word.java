14eccd99e321206dcc5817c5c0500916f71f5de6
DamianGrudzien <grudzien1694@gmail.com>
2022-08-21 18:17:29 +0200

    properties-for-char logback-added

--- src/main/java/com/passwordgenerator/damiangrudzien/model/Word.java
+++ src/main/java/com/passwordgenerator/damiangrudzien/model/Word.java
@@ -1,8 +1,10 @@
 package com.passwordgenerator.damiangrudzien.model;
 
 import lombok.*;
+import org.hibernate.Hibernate;
 
 import javax.persistence.*;
+import java.util.Objects;
 
 @Entity
 @Getter
@@ -20,4 +22,16 @@ public class Word {
 
     public Word(){}
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Word word = (Word) o;
+        return id != null && Objects.equals(id, word.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
