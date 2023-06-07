aeaf239fe5297599e80eb40d76580936ec95073b
Damian Grudzień <grudzien1694@gmail.com>
2023-06-07 17:00:58 +0200

    DG - Implementing Redis Cache

--- src/main/java/com/passwordgenerator/damiangrudzien/Application.java
+++ src/main/java/com/passwordgenerator/damiangrudzien/Application.java
@@ -2,9 +2,10 @@ package com.passwordgenerator.damiangrudzien;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class);
