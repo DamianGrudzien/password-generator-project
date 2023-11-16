7eea9c0b50b86c17bca7c6b6143971167a48b5ef
Damian Grudzień <grudzien1694@gmail.com>
2023-11-16 17:06:02 +0100

    update cors

---
+++ src/main/java/com/passwordgenerator/damiangrudzien/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.passwordgenerator.damiangrudzien.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+        registry.addMapping("https://damiangrudzien.github.io/password-generator-react-app/**");
+    }
+}
